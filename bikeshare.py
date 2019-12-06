79cdc4fc2ff5f4abddbb2443e5f1677de83bab33
Bernhard Ferber <bernhard.ferber@bmw.de>
2019-12-06 13:25:27 +0100

    Updated Bikeshare.py restart question

--- bikeshare.py
+++ bikeshare.py
@@ -197,7 +197,7 @@ def main():
                 break
         
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Please enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
